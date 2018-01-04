e67cd3b5a0fe56a2f62d6a6f7ed4e91ac1ac87e0
zouhair <zouhair@zee.aero>
2018-01-04 14:17:04 -0800

    initial commit

---
+++ loc.py
@@ -0,0 +1,83 @@
+import json
+from datetime import datetime
+from countries import countries
+from tqdm import tqdm
+
+import numpy as np
+
+startdate = datetime.strptime('2013 Mar 14', '%Y %b %d');
+
+cc = countries.CountryChecker('./countries/TM_WORLD_BORDERS-0.3.shp')
+
+def setDate(loc):
+	ts_ms = int(loc['timestampMs']) / 1000
+	loc['date'] = datetime.fromtimestamp(ts_ms)
+
+def setCountry(loc):
+	lat = loc['latitudeE7']/1e7
+	lon = loc['longitudeE7']/1e7
+	country = cc.getCountry(lat, lon)
+	if country:
+		loc['country'] = country.name
+	else:
+		loc['country'] = '?'
+
+
+print "Reading Data"
+with open('LocationHistory.json') as fp:
+	data = json.load(fp)
+	locHist = data['locations']
+
+print "Pruning Data"
+dayHist = []
+prevDate = datetime.now()
+for idx, loc in tqdm(enumerate(locHist)):
+	setDate(loc)
+	if loc['date'] < startdate:
+		break
+
+	dt = prevDate - loc['date']
+	if(dt.days >= 1):
+		dayHist.append(loc)
+		prevDate = loc['date']
+
+
+
+print "Finding out countries"
+for loc in tqdm(dayHist):
+	setCountry(loc)
+
+print "Compressing"
+dates     = np.array([x['date'] for x in reversed(dayHist)])
+countries = np.array([x['country'] for x in reversed(dayHist)])
+
+idx = np.where(countries[:-1] != countries[1:])[0]
+D = dates[idx]
+C = countries[idx]
+
+outFile = open('locHist.csv','w')
+totalDays = 0
+totalNZDays = 0
+p = startdate
+for i in range(len(D)):
+	c = C[i]
+	d = D[i]
+	dt = (d - p);
+	outStr = "{cnt}, {st}, {en}, {d}".format(cnt=c, st=p.strftime('%m/%d/%y'), en=d.strftime('%m/%d/%y'), d=dt.days)
+	p = d
+
+	print outStr
+	outFile.write(outStr+"\n")
+
+	if not c in ['United States', 'Puerto Rico']:
+		totalDays += dt.days + 1
+
+	if c in ['New Zealand']:
+		totalNZDays += dt.days + 1
+
+	break
+	
+outFile.close()
+
+print 'Total days outside of US', totalDays
+print 'Total days in NZ', totalNZDays
